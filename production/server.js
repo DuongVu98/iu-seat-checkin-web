24f4d06f3fb4405a0161219645dc4ac2735d2126
Tung Duong <duongvutung1998@gmail.com>
2020-09-03 04:14:47 +0700

    clear production package

--- production/server.js
+++ production/server.js
@@ -1,19 +1,17 @@
 var path = require("path");
 var express = require("express");
-var subdomain = require("express-subdomain");
 var router = express.Router();
 
 var DIST_DIR = path.join(__dirname, "../dist");
 var PORT = 8080;
-var app = express();
 
-//Serving the files on the dist folder
+var app = express();
 app.use(express.static(DIST_DIR));
 
-//Send index.html when the user access the web
 router.get("*", function(req, res) {
     res.sendFile(path.join(DIST_DIR, "index.html"));
 });
-app.use(subdomain("seat-checkin", router));
+
+app.use(router);
 
 app.listen(PORT);
