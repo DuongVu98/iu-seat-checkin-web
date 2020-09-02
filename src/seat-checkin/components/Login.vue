2cc3ecb48444481e37d8e2de743668c52e5f4498
Tung Duong <duongvutung1998@gmail.com>
2020-09-02 18:33:15 +0700

    design login ui

--- src/seat-checkin/components/Login.vue
+++ src/seat-checkin/components/Login.vue
@@ -2,52 +2,64 @@
     <div class="wrapper">
         <div class="login-page-background"></div>
         <div class="login-main-component">
-            <v-row style="height: 100%;">
-                <v-col cols="6">
-                    <div class="both-sides">
-                        <div class="logo-block">
-                            <img src="@/assets/images/single-logo.png" alt="" />
-                        </div>
-                    </div>
-                </v-col>
-                <v-col cols="6">
-                    <div class="both-sides">
-                        <div class="login-block">
-                            <v-card>
-                                <v-toolbar color="primary" dark flat>
-                                    <v-toolbar-title>Admin login</v-toolbar-title>
-                                </v-toolbar>
-                                <v-card-text>
-                                    <div class="main-form">
-                                        <v-form>
-                                            <v-text-field
-                                                label="Username"
-                                                class="input-group--focused"
-                                                prepend-icon="mdi-account"
-                                                outlined
-                                                rounded
-                                                required
-                                            ></v-text-field>
-                                            <v-text-field
-                                                :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
-                                                :type="hidePassword ? 'text' : 'password'"
-                                                label="Password"
-                                                class="input-group--focused"
-                                                prepend-icon="mdi-lock"
-                                                outlined
-                                                rounded
-                                                required
-                                                @click:append="hidePassword = !hidePassword"
-                                            ></v-text-field>
-                                            <v-btn :disabled="!valid" color="success" class="mr-4">Login</v-btn>
-                                        </v-form>
-                                    </div>
-                                </v-card-text>
-                            </v-card>
-                        </div>
-                    </div>
-                </v-col>
-            </v-row>
+            <v-card shaped class="rounded-xl page-card" elevation="24">
+                <div class="inside-card">
+                    <v-row style="height: 100%;">
+                        <v-col cols="6">
+                            <div class="both-sides">
+                                <div class="logo-block">
+                                    <img src="@/assets/images/single-logo.png" alt="" />
+                                </div>
+                            </div>
+                        </v-col>
+                        <v-col cols="6">
+                            <div class="both-sides">
+                                <div class="login-block">
+                                    <v-card class="rounded-lg">
+                                        <v-toolbar color="primary" dark flat>
+                                            <v-toolbar-title>Admin login</v-toolbar-title>
+                                        </v-toolbar>
+                                        <v-card-text>
+                                            <div class="main-form">
+                                                <v-form>
+                                                    <v-text-field
+                                                        label="Username"
+                                                        class="input-group--focused"
+                                                        v-model="username"
+                                                        prepend-icon="mdi-account"
+                                                        outlined
+                                                        rounded
+                                                        required
+                                                    ></v-text-field>
+                                                    <v-text-field
+                                                        :append-icon="hidePassword ? 'mdi-eye' : 'mdi-eye-off'"
+                                                        :type="hidePassword ? 'text' : 'password'"
+                                                        label="Password"
+                                                        class="input-group--focused"
+                                                        v-model="password"
+                                                        prepend-icon="mdi-lock"
+                                                        outlined
+                                                        rounded
+                                                        required
+                                                        @click:append="hidePassword = !hidePassword"
+                                                    ></v-text-field>
+                                                    <v-btn
+                                                        :disabled="!valid"
+                                                        color="success"
+                                                        class="mr-4"
+                                                        @click="doLogin()"
+                                                        >Login</v-btn
+                                                    >
+                                                </v-form>
+                                            </div>
+                                        </v-card-text>
+                                    </v-card>
+                                </div>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </div>
+            </v-card>
         </div>
     </div>
 </template>
@@ -62,6 +74,9 @@ export default {
             valid: true,
         };
     },
+    methods: {
+        doLogin() {},
+    },
 };
 </script>
 <style>
@@ -69,27 +84,33 @@ export default {
     height: 100%;
 }
 .login-page-background {
-    background-image: url("https://lh3.googleusercontent.com/proxy/tTk3Z2hVPB2Zzn-qykhqwnY4HUrj12DbJBq_xwWpAjh2HTnazHjl5H2BhQ9ZbHibee8NAqWT-vEP4uRpd44Wz02PxlZkLdckL-dhAovEYroBKFHYlkL--JCnIm2jFJCQtscv4wia-pOX1e6RUmzEm9P0QWk");
-    filter: blur(8px);
-    -webkit-filter: blur(8px);
+    background: url("../../assets/images/login-background.jpg");
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
     height: 100%;
+    /* filter: blur(8px);
+    -webkit-filter: blur(8px); */
 }
 .login-main-component {
-    background-image: linear-gradient(to bottom right, #153f6f, #0ab0da);
-    border: 1px solid #bfbfbf;
-    border-radius: 50px;
-    box-shadow: 5px 5px 3px #636363;
-    height: 80%;
-    width: 75%;
+    height: 70%;
+    width: 70%;
 
     position: absolute;
     z-index: 2;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
+}
+.page-card {
+    height: 100%;
+}
+.inside-card {
+    background: url("../../assets/images/login-foreground.png");
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 100%;
 }
 .both-sides {
     display: flex;
