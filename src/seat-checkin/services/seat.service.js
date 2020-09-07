e9fd82addeb05a4445d427a1da3f9c8ca60ea3e7
Tung Duong <duongvutung1998@gmail.com>
2020-09-07 16:31:47 +0700

    headers for secured api

--- src/seat-checkin/services/seat.service.js
+++ src/seat-checkin/services/seat.service.js
@@ -6,22 +6,30 @@ const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PATCH, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Origin, Content-Type, X-Auth-Token",
+    user: JSON.parse(localStorage.getItem("vuex")).seatCheckinState.userAccount.id,
 };
+
+const requestInstance = axios.create({
+    headers: {
+        get: headers,
+        post: headers,
+    },
+});
 
 export default {
     getAllSeat() {
-        return axios.get(`${backendServer}/viewer/all-seats`, headers);
+        return requestInstance.get(`${backendServer}/viewer/all-seats`);
     },
     addOccupiedSeat(seat) {
-        return axios.post(`${backendServer}/admin/add-seat`, seat);
+        return requestInstance.post(`${backendServer}/admin/add-seat`, seat);
     },
     updateSeatCode(seatData) {
-        return axios.post(`${backendServer}/admin/update-seat`, seatData);
+        return requestInstance.post(`${backendServer}/admin/update-seat`, seatData);
     },
     deleteSeatCode(seatId) {
-        return axios.get(`${backendServer}/admin/delete-seat/${seatId}`);
+        return requestInstance.get(`${backendServer}/admin/delete-seat/${seatId}`);
     },
     updateSeatOccupied(updateData) {
-        return axios.post(`${backendServer}/admin/set-occupied`, updateData);
+        return requestInstance.post(`${backendServer}/admin/set-occupied`, updateData);
     },
 };
