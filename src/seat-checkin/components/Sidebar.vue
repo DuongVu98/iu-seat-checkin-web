38d86b587a788ccac34680a385c73baa2f235ec7
Tung Duong <duongvutung1998@gmail.com>
2020-09-08 00:29:12 +0700

    vocab stuff

--- src/seat-checkin/components/Sidebar.vue
+++ src/seat-checkin/components/Sidebar.vue
@@ -9,7 +9,7 @@
                         </v-list-item-avatar>
 
                         <v-list-item-content>
-                            <v-list-item-title>Admin Direction</v-list-item-title>
+                            <v-list-item-title>Admin Directions</v-list-item-title>
                             <!-- <v-list-item-subtitle>Subtext</v-list-item-subtitle> -->
                         </v-list-item-content>
                     </v-list-item>
